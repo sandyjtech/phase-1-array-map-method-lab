f668bdf6e020c47abf8e78ac9759938087bc6a9c
Sandra G <sandyjtech@gmail.com>
2023-06-19 11:47:12 -0700

    mapped thry words and cap() first letter

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  const newTutorials = tutorials.map((tutorials) => {
+    //we use split to get the words
+    const words = tutorials.split(" ");
+    //we use map to change the first letter to uppercase
+    const fixedWords = words.map((word) => {
+      //we use charAt to get the first letter
+      const fixedLetters = word.charAt(0).toUpperCase();
+      //we use slice to remove the first letter
+      const reWriteWord = word.slice(1);
+      return fixedLetters + reWriteWord;
+    });
+    //we use join to join the words back together
+    return fixedWords.join(" ");
+  });
+  //
+  return newTutorials;
+};
